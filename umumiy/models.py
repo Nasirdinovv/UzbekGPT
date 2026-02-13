65c2bf451fd5c8cf5d2a1db52d8e9aec880d2aff
Nasriddinovv Asadulloh <doniyornasirdinov82@gmail.com>
2026-02-13 20:23:23 +0500

    models yangilandi keyin o'zgartilinish kerak

--- umumiy/models.py
+++ umumiy/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    fullname = models.CharField( max_length=50)
+    age = models.PositiveIntegerField()
+    
+    gmail = models.EmailField(, max_length=254)
+    password = models.CharField()
+
+
+class uzbekGPT(models.Model):
+    pass
+
+
+
+class histories(models.Model):
+    history = models.TextField()
+    

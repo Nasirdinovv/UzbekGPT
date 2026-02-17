52c0741d3b158d6e091a6b086473f02cf1dbf058
Nasriddinovv Asadulloh <doniyornasirdinov82@gmail.com>
2026-02-17 22:18:49 +0500

    admin py to'g'irland

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
 
 
+AUTH_USER_MODEL = "umumiy.User_profile"
--- umumiy/admin.py
+++ umumiy/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User_profile, Ai_agent, History
 # Register your models here.
+admin.site.register(User_profile)
+admin.site.register(Ai_agent)
+admin.site.register(History)

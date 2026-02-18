fa3b54d98ad29db125509b69ad81e8bcfc6c2063
Nasriddinovv Asadulloh <doniyornasirdinov82@gmail.com>
2026-02-18 10:37:35 +0500

    models.py tuzatilindi !

--- roadmi.md
+++
@@ -1,8 +0,0 @@
-# UzbekGPT -> bu nima qila oladi ? ->
-#   unda tarixiy, matematik, suhbatlashish va odamga yoqadigan gaplarni yozishadigan AI chat agent
-
-
-
-1 - user kirganda regiter yoki login bo'limidan o'tishi kerak
-2 - Dashboard ga o'tganda AI dan ro'lni tanlash kerek 
-3 - 
--- umumiy/models.py
+++ umumiy/models.py
@@ -1,16 +1,50 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+# <<< User_profile modellari >>> #
+class UserProfile(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
+# <<< Ai_agent modellari >>> #    
+class Ai_agent(models.Model):
+    name = models.CharField(max_length=100)
+    kasbi = models.CharField(max_length=100)
+    shaxsiyati = models.TextField()
+    icon = models.CharField(max_length=255)  # Ikonka URL'si
+    def __str__(self):
+        return f"{self.name} - {self.kasbi}"
+    
 
 
+# <<< History modellari >>> #
+class History(models.Model):
+    KIMDAN = [
+        ('user', 'User'),
+        ('agent', 'Agent'),
+    ]
 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    agent = models.ForeignKey(Ai_agent, on_delete=models.CASCADE)
+    text = models.TextField()
 
+    kimdan = models.CharField(choices=KIMDAN, max_length=10)
 
-
-
-
-
-
-
+    def __str__(self):
+        return f"{self.user.email} - {self.agent.name} - {self.kasi }"
+    
 
 
 

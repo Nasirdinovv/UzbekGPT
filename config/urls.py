50425bb375e589d036db8e332ce0eca2dbb4c57f
Nasriddinovv Asadulloh <doniyornasirdinov82@gmail.com>
2026-02-26 22:11:52 +0500

    hamma narsalarni yangiladim

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from umumiy.views import login_user, index, chat, logout_user
+from umumiy.views import login_user, index, chat, logout_user, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login_user, name='login_user'),
     path('chat/', chat, name='chat'),
     path('logout/', logout_user, name='logout_user'),
+    path('register/', register, name='register_user'),
 ]
--- umumiy/views.py
+++ umumiy/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from umumiy.models import UserProfile
+
 
 def index(request):
     # Bu yerda hech qanday redirect kerak emas, shunchaki index.html ni ochamiz
@@ -26,12 +29,49 @@ def login_user(request):
     
     return render(request, "login.html")
 
+
+def logout_user(request):
+    logout(request)
+    return redirect("index")
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("index")
+    
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password_confirm = request.POST.get("password2")
+
+        if password != password_confirm:
+            messages.error(request, "Parollar mos kelmadi")
+            return redirect("register_user")
+
+        
+        if UserProfile.objects.filter(email=email).exists():
+            messages.error(request, "Bu email allaqachon ro'yxatdan o'tgan")
+            return redirect("register_user")
+        try:
+            # UserProfile orqali foydalanuvchi yaratish
+            new_user = UserProfile.objects.create_user(
+                email=email, 
+                password=password, 
+                first_name=first_name, 
+                last_name=last_name,
+            )
+            messages.success(request, "Muvaffaqiyatli ro'yxatdan o'tdingiz!")
+            return render(request, "register.html")
+
+        except Exception as e:
+            messages.error(request, f"Xatolik: {e}")
+            return redirect("register_user")
+
+    return render(request, "register.html")
+
 def chat(request):
     # Chatga faqat login qilganlar kirishi uchun
     if not request.user.is_authenticated:
         return redirect("login_user")
     return render(request, "chat.html")
-
-def logout_user(request):
-    logout(request)
-    return redirect("index")
